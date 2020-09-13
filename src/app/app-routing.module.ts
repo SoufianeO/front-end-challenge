8ab0d7d54b4a688567771c4afb9be1eef89ee655
soufiane ounida <soufianeounidainfo@gmail.com>
2020-09-14 00:11:42 +0100

    add routes to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RepoListComponent } from './component/repo-list/repo-list.component';
+import { RepoElementComponent } from './component/repo-element/repo-element.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'Repos/:page', component:RepoListComponent},
+  {path: 'Repo/:id', component:RepoElementComponent},
+  {path:'', redirectTo: 'Repos/1', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RepoListComponent } from './component/repo-list/repo-list.component';
@@ -14,7 +15,8 @@ import { RepoElementComponent } from './component/repo-element/repo-element.comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
