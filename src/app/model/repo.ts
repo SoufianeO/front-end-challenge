6c17efae1240aafa4e0982fcd9135457b2278c2b
soufiane ounida <soufianeounidainfo@gmail.com>
2020-09-14 01:44:32 +0100

    fix Repo model

--- src/app/model/repo.ts
+++ src/app/model/repo.ts
@@ -1,5 +1,5 @@
 export class Repo {
-    constructor(public RepositoryName: string, public description: string,
+    constructor(public repoName: string, public description: string,
         public userName: string, public imagePath: string,
         public stars: number, public issues: number) {}
 }
