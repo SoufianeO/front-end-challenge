a895b873ce85fcd3be99fec6a0ba7942a6a77ce8
soufiane ounida <soufianeounidainfo@gmail.com>
2020-09-14 01:42:53 +0100

    create model repo.ts

--- src/app/model/repo.ts
+++ src/app/model/repo.ts
@@ -1,2 +1,5 @@
 export class Repo {
+    constructor(public RepositoryName: string, public description: string,
+        public userName: string, public imagePath: string,
+        public stars: number, public issues: number) {}
 }
